a0191cd3faccd0fa50395a7a9eaac5e6d078deb7
Muhammad Nur Fikri <mf4853432@gmail.com>
2023-04-18 14:50:05 +0700

    Tugas 8 Javascript
    Pengumpulan Tugas Niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function hewanTernak(){
+    var sapi = {
+        warna : "hitam putih",
+        berat : 250,
+        kondisi : "sehat",
+    }
+
+    sapi.berat = 400
+    console.log(sapi)
+    console.log(sapi.warna)
+}
+hewanTernak()
